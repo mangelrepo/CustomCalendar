6c74532b9af1f7d28109d0dd3e2aab293c0ad7da
desarrollo11 <miguelangelgonzalez@dosatic.com>
2020-05-21 11:31:59 +0200

    Commit inicial

---
+++ app/src/main/java/com/mangelrepo/customcalendar/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.mangelrepo.customcalendar
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ScrollView
+import android.widget.TextView
+import com.mangelrepo.customcalendar.data.Day
+
+import com.mangelrepo.customcalendar.widget.CustomCalendar
+
+import java.util.Calendar
+import java.util.GregorianCalendar
+
+
+class MainActivity : AppCompatActivity(){
+    companion object{
+        var TAG = "MainActivity";
+    }
+
+    lateinit var customCalendar:CustomCalendar
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar!!.elevation = 0f
+        window.statusBarColor = resources.getColor(R.color.google_red)
+        var relativeLayout = findViewById<ScrollView>(R.id.scrollView)
+        var textView = findViewById<TextView>(R.id.tv_date)
+
+        customCalendar = findViewById(R.id.collapsibleCalendarView)
+        /*
+        collapsibleCalendar.setOnSwipeTouchListener(object:OnSwipeTouchListener(this@MainActivity){
+            override fun onSwipeRight() {
+                collapsibleCalendar.nextDay()
+            }
+
+            override fun onSwipeLeft() {
+                collapsibleCalendar.prevDay()
+            }
+
+            override fun onSwipeTop() {
+                if(collapsibleCalendar.expanded){
+                    collapsibleCalendar.collapse(300)
+                }
+            }
+
+            override fun onSwipeBottom() {
+                if(!collapsibleCalendar.expanded){
+                    collapsibleCalendar.expand(300)
+                }
+            }
+        });
+        */
+        //To hide or show expand icon
+        customCalendar.setExpandIconVisible(false)
+        val today = GregorianCalendar()
+        customCalendar.addEventTag(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH))
+        today.add(Calendar.DATE, 1)
+        customCalendar.selectedDay = Day(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH))
+        customCalendar.addEventTag(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH), Color.BLUE)
+        customCalendar.addEventTag(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH), Color.RED)
+        customCalendar.params = CustomCalendar.Params(0, 100)
+        customCalendar.setCalendarListener(object : CustomCalendar.CalendarListener {
+            override fun onDayChanged() {
+
+            }
+
+            override fun onClickListener() {
+                if(customCalendar.expanded){
+                    customCalendar.collapse(400)
+                }
+                else{
+                    customCalendar.expand(400)
+                }
+            }
+
+            override fun onDaySelect() {
+                textView.text = customCalendar.selectedDay?.toUnixTime().toString()
+            }
+
+            override fun onItemClick(v: View) {
+
+            }
+
+            override fun onDataUpdate() {
+
+            }
+
+            override fun onMonthChange() {
+
+            }
+
+            override fun onWeekChange(position: Int) {
+
+            }
+        })
+
+
+    }
+}
